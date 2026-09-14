ace6fccdf141a6bba77cefe544f93e6d5b300a89
Igor <igorv88@gmail.com>
2026-09-14 09:55:46 -0300

    fix: align Freeze Last Seen tile state

--- app/src/main/java/com/waenhancer/services/FreezeLastSeenTileService.java
+++ app/src/main/java/com/waenhancer/services/FreezeLastSeenTileService.java
@@ -3,7 +3,7 @@ package com.waenhancer.services;
 public class FreezeLastSeenTileService extends BaseTileService {
     @Override
     protected String getPreferenceKey() {
-        return "freeze_last_seen_actual";
+        return "freezelastseen";
     }
 
     @Override
